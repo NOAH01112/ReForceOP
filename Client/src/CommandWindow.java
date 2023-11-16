bbb01fa65f8ea359fe6fd8a59160ac7e5fb6df78
ParkMini <parkteammini@gmail.com>
2023-11-16 12:22:39 +0900

    명령어 사용 히스토리 및 전송 오류 수정

--- Client/src/CommandWindow.java
+++ Client/src/CommandWindow.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -9,41 +11,75 @@ import java.net.Socket;
 public class CommandWindow {
     private JFrame frame;
     private JTextField commandTextField;
-    private JButton sendButton;
+    private JTextArea historyTextArea;
     private Socket socket;
+    private PrintWriter writer;
 
     public CommandWindow(Socket socket) {
         this.socket = socket;
+        if (!initializeWriter()) {
+            JOptionPane.showMessageDialog(null, "서버와의 연결에 실패했습니다.");
+            return;
+        }
 
         frame = new JFrame("Send Command");
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new BorderLayout());
 
-        commandTextField = new JTextField(20);
-        sendButton = new JButton("Send");
-
-        sendButton.addActionListener(new ActionListener() {
+        commandTextField = new JTextField();
+        commandTextField.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 sendCommand();
             }
         });
 
-        frame.add(commandTextField);
-        frame.add(sendButton);
+        historyTextArea = new JTextArea(10, 30);
+        historyTextArea.setEditable(false);
+
+        frame.add(new JScrollPane(historyTextArea), BorderLayout.CENTER);
+        frame.add(commandTextField, BorderLayout.SOUTH);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                closeResources();
+            }
+        });
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
+    private boolean initializeWriter() {
+        try {
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     private void sendCommand() {
-        if (socket != null && !socket.isClosed()) {
-            try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-                out.println(commandTextField.getText());
-                commandTextField.setText("");
-            } catch (IOException e) {
-                e.printStackTrace();
+        String command = commandTextField.getText();
+        if (!command.isEmpty()) {
+            writer.println(command);
+            historyTextArea.append(command + "\n");
+            commandTextField.setText("");
+        }
+    }
+
+    private void closeResources() {
+        if (writer != null) {
+            writer.close();
+        }
+        try {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
