530b1269db115b604d8b2d5742004e77a44389bb
ParkMini <parkteammini@gmail.com>
2023-11-16 12:15:08 +0900

    서버 연결 창

---
+++ Client/src/ServerConnectWindow.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ServerConnectWindow {
+    private JFrame frame;
+    private JTextField ipTextField, portTextField;
+    private JButton connectButton;
+
+    public ServerConnectWindow() {
+        frame = new JFrame("Connect to Server");
+        frame.setLayout(new FlowLayout());
+
+        ipTextField = new JTextField(15);
+        portTextField = new JTextField(5);
+        connectButton = new JButton("Connect");
+
+        connectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                connectToServer();
+            }
+        });
+
+        frame.add(new JLabel("Server IP:"));
+        frame.add(ipTextField);
+        frame.add(new JLabel("Port:"));
+        frame.add(portTextField);
+        frame.add(connectButton);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void connectToServer() {
+        String ip = ipTextField.getText();
+        int port = Integer.parseInt(portTextField.getText());
+
+        try {
+            Socket socket = new Socket(ip, port);
+            new CommandWindow(socket);
+            frame.dispose();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(frame, "서버에서 ReForceOp가 실행되고 있지 않은 것 같습니다.");
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(ServerConnectWindow::new);
+    }
+}
